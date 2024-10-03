9d1c72a73c386262f63ee15084ca949799f3c70f
Krisztian16 <55728934+Krisztian16@users.noreply.github.com>
2024-10-03 18:03:01 +0200

    Add files via upload

--- array.go
+++ array.go
@@ -17,4 +17,22 @@ func main() {
 	arr2D := [2][3]int{{1, 3, 5}, {2, 4, 6}}
 	fmt.Println(arr2D)
 	fmt.Println(arr2D[1][2])
+
+	var a []int = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(a)
+	a = append(a, 10)
+
+	fmt.Println(a)
+
+	var b [6]int = [6]int{11, 22, 33, 44, 55, 66}
+	fmt.Println(b)
+	fmt.Println(cap(b))
+	c := b[0:3]
+	fmt.Println(c)
+	d := b[3:5]
+	fmt.Println(d)
+	fmt.Println(cap(d))
+	d = append(d, b[5])
+	fmt.Println(d)
+
 }
