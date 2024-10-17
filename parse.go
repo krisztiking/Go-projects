51ba3d95777953d5cc3d5f25d996334b3f25ca4e
Krisztian Domokos <domokoskrisztian7@gmail.com>
2024-10-17 18:36:29 +0200

    Href and text parsing

--- parse.go
+++ parse.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"io"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -17,12 +17,56 @@ func Parse(r io.Reader) ([]Link, error) {
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println(doc[2], "\n\n")
-	dfs(doc, "")
-	return nil, nil
+	// fmt.Println(doc, "\n\n")
+
+	nodes := linkNodes(doc)
+	var links []Link
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
+	}
+	// dfs(doc, "")
+
+	return links, nil
 }
 
-func dfs(n *html.Node, padding string) {
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+	ret.Text = text(n)
+	return ret
+}
+
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c)
+	}
+	return strings.Join(strings.Fields(ret), " ")
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+	}
+	return ret
+}
+
+/* func dfs(n *html.Node, padding string) {
 	msg := n.Data
 	if n.Type == html.ElementNode {
 		msg = "<" + msg + ">"
@@ -31,4 +75,4 @@ func dfs(n *html.Node, padding string) {
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		dfs(c, padding+"  ")
 	}
-}
+} */
